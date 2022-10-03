7b441e51e46c6f1795a1a51bbe46434187491fac
김지성 <77275224+adk2421@users.noreply.github.com>
2022-10-04 01:10:53 +0900

    LOL Data 정보 호출 API 작성

---
+++ src/main/java/com/kalamba/api/ChampionInfoAPI.java
@@ -0,0 +1,36 @@
+package com.kalamba.api;
+
+import java.util.Map;
+
+import org.json.simple.parser.ParseException;
+
+public class ChampionInfoAPI {
+
+    API API = new API();
+
+    // LOL Data Version
+    @SuppressWarnings("unchecked")
+    public String getDataVer(String type) throws ParseException {
+        String url = "https://ddragon.leagueoflegends.com/realms/kr.json"; // LOL 기본 Data Version API URL
+
+        Map<String, Object> result = API.callAPI(url);
+
+        Map<String, Object> dataVersion = (Map<String, Object>) result.get("n");
+
+        return dataVersion.get(type).toString();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public String getChampInfo(String championName) throws ParseException {
+        String dataVersion = getDataVer("champion");
+        String url = "https://ddragon.leagueoflegends.com/cdn/" + dataVersion + "/data/ko_KR/champion/"+ championName + ".json"; // 챔피언 정보 API URL
+
+        Map<String, Object> result = API.callAPI(url);
+
+        result = (Map<String, Object>) result.get("data");
+
+        result = (Map<String, Object>) result.get(championName);
+
+        return result.get("name").toString();
+    }        
+}
