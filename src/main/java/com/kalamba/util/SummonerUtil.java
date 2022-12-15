a885ee17e727b0c95424d7e7de8563423593312a
adk2421@gmail.com <adk2421@gmail.com>
2022-12-15 15:05:12 +0900

    feat: Add selectPlayerInfo (전적에서 대상 정보 추출)

--- src/main/java/com/kalamba/util/SummonerUtil.java
+++ src/main/java/com/kalamba/util/SummonerUtil.java
@@ -1,5 +1,8 @@
 package com.kalamba.util;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class SummonerUtil {
@@ -8,7 +11,13 @@ public class SummonerUtil {
     // API Key
     final String API_KEY = dotenv.get("API_AUTH_KEY");
 
-    // LOL API URL 만들기
+    /**
+     * 📢[ API URL 생성 ]
+     * @param region
+     * @param summonerName
+     * @param addURL
+     * @return
+     */
     public String makeURL(String region, String summonerName, String addURL) {
         String URL = "";
         String ASIA_BASEURL = dotenv.get("API_AUTH_ASIA_BASEURL");
@@ -30,6 +39,11 @@ public class SummonerUtil {
         return URL;
     }
 
+    /**
+     * 📢[ 초 단위로 받은 시간 데이터 변환 ]
+     * @param second
+     * @return
+     */
     public String timeFommater(int second) {
         String time = "";
 
@@ -41,5 +55,19 @@ public class SummonerUtil {
         time += second % 60 < 10 ? "0" + second % 60 : second % 60;
         
         return time;
+    }
+
+    /**
+     * 📢[ 게임 전적에서 대상 데이터 추출 ]
+     * @param participants
+     * @param userPID
+     * @return
+     */
+    public Map<String, Object> selectPlayerInfo(ArrayList<Map<String, Object>> participants, String userPID) {
+        for (Map<String, Object> participant : participants) {
+            if (userPID.equals(participant.get("puuid")))
+                return participant;
+        }
+        return null;
     }
 }
