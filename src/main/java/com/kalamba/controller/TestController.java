7c8083ee6c2dbf3cb47fb66f4ddabf6b3927306f
adk2421 <adk2421@gmail.com>
2023-06-23 18:58:43 +0900

    add: Test JPA CRUD

--- src/main/java/com/kalamba/controller/TestController.java
+++ src/main/java/com/kalamba/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.kalamba.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +33,15 @@ public class TestController {
     }
 
     /**
+     * ID 데이터 조회
+     * @return
+     */
+    @GetMapping("select-id")
+    public Optional<TestEntity> findData() {
+        return testRepository.findById(5L);
+    }
+
+    /**
      * 데이터 입력
      * @return
      * Error: The database returned no natively generated identity value - PK가 AUTO_INCREMENT 속성으로 되어있지 않아서 발생
@@ -36,8 +49,30 @@ public class TestController {
     @PostMapping("insert")
     public TestEntity insertData() {
         List<TestEntity> data = findAllData();
-        final TestEntity testEntity = TestEntity.builder().testNumber(403).testText("Insert Test " + data.get(data.size()-1).getTestNumber()).build();
+        final TestEntity testEntity = TestEntity.builder().testText("Insert Test " + data.get(data.size()-1).getTestNumber()).build();
         
         return testRepository.save(testEntity);
     }
+
+    /**
+     * 데이터 수정 - 사용자 정의 쿼리
+     * @return
+     * Error: For queries with named parameters you need to use provide names for method parameters. - Repository에 파라미터를 명시해주지 않음
+     */
+    @Transactional
+    @PutMapping("update")
+    public int updateData() {
+        List<TestEntity> data = findAllData();
+        return testRepository.updateData((int)data.get(data.size()-1).getTestNumber(), "UPDATE TEST" + data.get(data.size()-1).getTestNumber());
+    }
+
+    /**
+     * 조건 데이터 삭제
+     * @return
+     */
+    @DeleteMapping("delete")
+    public void deleteData() {
+        final TestEntity testEntity = TestEntity.builder().testNumber(7).testText("Insert Test5").build();
+        testRepository.delete(testEntity);
+    } 
 }
--- src/main/java/com/kalamba/repository/TestRepository.java
+++ src/main/java/com/kalamba/repository/TestRepository.java
@@ -1,6 +1,9 @@
 package com.kalamba.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.kalamba.entity.TestEntity;
@@ -8,4 +11,8 @@ import com.kalamba.entity.TestEntity;
 @Repository
 public interface TestRepository extends JpaRepository<TestEntity, Long> {
     
+    // 데이터 수정
+    @Modifying(clearAutomatically = true)
+    @Query("update test_db set test_number = :number+100, test_text = :text where test_number = :number")
+    int updateData(@Param("number") int number, @Param("text") String text);
 }
